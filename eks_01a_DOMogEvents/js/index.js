5c33812686b68de4c88f17dfb9eaff9e7e6cc3c0
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2021-08-17 12:53:11 +0200

    Added js button counter

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -1,7 +1,28 @@
-
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+	// operet en funktion som indsætter tallet 5, i p-tagget med id'et 'text_count'.
+	// Nå man klikker på plusset skal værdien formindseks med en
+	// værdien må ikke kommer over 10
+	// værdien må ikke kommer over 0
 
 	// skriv din kode her...
-
+	const buttonUp = document.querySelector("#btn-count-increase");
+	const buttonDown = document.querySelector("#btn-count-decrease");
+	const textFinder = document.querySelector("#text-count");
+	let countUp = 5;
+	textFinder.textContent = countUp + "🕺";
+	buttonUp.addEventListener("click", count);
+	buttonDown.addEventListener("click", countNed);
+	function count() {
+		if (countUp < 10) {
+			countUp += 1;
+			textFinder.textContent = countUp;
+		}
+	}
+	function countNed() {
+		if (countUp > 0) {
+			countUp -= 1;
+			textFinder.textContent = countUp;
+		}
+	}
 }); // Afslutter: DOMContentLoaded
