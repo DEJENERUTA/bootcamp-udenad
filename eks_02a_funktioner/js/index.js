0d1484a63a4bf7466b6fda0257bd7d0159a11efd
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2021-08-19 10:40:35 +0200

    Added how to create a function and see the result in console, with out html

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -1,6 +1,12 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
-
+	//opret en funktion som du kalder 'hils' og lad den udskrive et 'Hej med dig!' i konsolen.
+	//kald funktionen. husk paranteser.
+	//har det betydning om du kalder funktionen før eller efter den er oprettet?
 	// skriv din kode her...
 
+	function hils() {
+		console.log("Hej med dig!");
+	}
+	hils();
 }); // Afslutter: DOMContentLoaded
